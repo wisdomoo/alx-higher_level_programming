810267fc8451582c388bd4e1f144820d01bdfaf6
wisdomoo <okoroaforwisdomo@gmail.com>
2022-12-07 15:21:14 +0100

    files added

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > (len(my_list) - 1):
-        return (my_list)
-    del my_list[idx]
-    return (my_list)
+def mutiply_list_map(my_list=[], number=0):
+    return (list(map(lambda x: x * number, my_list)))
