c481a2ab764fce55db2792a57a30557e0f64dee5
wisdomoo <okoroaforwisdomo@gmail.com>
2022-12-07 15:05:58 +0100

    files added

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > (len(my_list) - 1):
-        return (my_list)
-    my_list[idx] = element
-    return (my_list)
+def uniq_add(my_list=[]):
+    s = set(my_list)
+    add = sum(s)
+    return (add)
