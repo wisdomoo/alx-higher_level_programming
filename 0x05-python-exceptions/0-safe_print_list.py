5e95ac183fff259db5ba7ae04c9ed55637f6110c
wisdomoo <okoroaforwisdomo@gmail.com>
2022-12-19 12:52:53 +0100

    correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-
-
 def safe_print_list(my_list=[], x=0):
-    """
-    prints a list of anything, but only prints the integers
-    Returns the amount of integers printed
-    """
-    printed = 0
-    for i in range(x):
+    count = 0
+    for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            printed += 1
-        except:
-            continue
+            count += 1
+        except IndexError:
+            pass
     print()
-    return printed
+    return count
